b9e23398fc9fd0f8c361e5e0b03b13bcada275d3
Róbert Klimant <roboklimant@gmail.com>
2024-09-11 23:27:31 +0200

    feat: animation for scroll texts

---
+++ src/utils/scrollAnimation.ts
@@ -0,0 +1,31 @@
+import SplitType from 'split-type';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+export const scrollAnimation = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const scrollEl = document.querySelectorAll('[scroll-animation]') as NodeListOf<HTMLElement>;
+  if (!scrollEl) return;
+  scrollEl.forEach((el) => {
+    let typeSplit = new SplitType(el, {
+      types: 'lines,words,chars',
+      tagName: 'span',
+    });
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: el,
+        start: 'top 90%',
+        end: 'bottom 40%',
+        scrub: true,
+      },
+    });
+    tl.from(el.querySelectorAll('.word'), {
+      opacity: 0.3,
+      duration: 2.5,
+      ease: 'power4.out',
+      stagger: { each: 0.3 },
+    });
+  });
+};
